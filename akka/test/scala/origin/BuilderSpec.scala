9a869772d99b4385f4e8023d7ab37065f0190a6d
Sanjin Sehic <ssehic@infosys.tuwien.ac.at>
2013-04-11 11:59:00 +0200

    Add missing akka.Builder spec

---
+++ akka/test/scala/origin/BuilderSpec.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2013 Sanjin Sehic
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package at.ac.tuwien.infosys
+package amber
+package akka
+package origin
+
+import amber.util.Type
+
+class BuilderSpec extends Spec("BuilderSpec")
+                  with BuilderComponent
+                  with OriginBehaviors.Local {
+
+  override type Origin[+A] = Origin.Local[A]
+
+  override protected type Configuration = BuilderComponent.Configuration
+  override protected object configuration extends BuilderComponent.Configuration {
+    override val system = BuilderSpec.this.system
+  }
+
+  override val fixture = new Fixture {
+    override def create[A: Type](name: Origin.Name, family: Origin.Family, read: Fixture.Read[A]) =
+      builder.build(name, family) {meta => read() map {value => (Origin.Value(name, value), meta)}}
+  }
+
+  "akka.OriginBuilder" should {
+    "build an origin" which {
+      behave like anOrigin
+    }
+  }
+}
