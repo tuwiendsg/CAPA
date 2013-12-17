d326469fdc1ac74e474f230b894921923fcd5da9
Sanjin Sehic <ssehic@infosys.tuwien.ac.at>
2013-12-17 09:27:45 +0100

    Access Raspberry Pi server

--- demo/src/scala/amber/demo/temperature/Client.scala
+++ demo/src/scala/amber/demo/temperature/Client.scala
@@ -20,6 +20,7 @@ package temperature
 
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
+import scala.sys.process._
 import scala.util.Try
 
 import com.typesafe.config.ConfigFactory
@@ -39,29 +40,39 @@ trait Client extends akka.System.Remote with Scheduling with Runnable {
   override protected def configuration: Configuration = _configuration
   private object _configuration extends Configuration
 
-  map[Double, Double]("temperature/celsius", "temperature/kelvin") {x => x + 273.15}
-  map[Double, Double]("temperature/celsius", "temperature/fahrenheit") {x => x * 9 / 5 + 3}
+  map[BigDecimal, BigDecimal]("temperature/celsius", "temperature/kelvin") {x => x + 273.15}
+  map[BigDecimal, BigDecimal]("temperature/celsius", "temperature/fahrenheit") {x => x * 9 / 5 + 3}
 
   override def client: Client = _client
   trait Client extends super.Client {
 
     val temperature = entity("Temperature")
     temperature.celsius = {
-      () => for {values <- temperature("celsius", at = "A")} yield Try {values.min}
+      () => for {values <- temperature("celsius")} yield Try {values.max}
     }
     temperature.kelvin = {
-      () => for {values <- temperature("kelvin", at = "B")} yield Try {values.max}
-    }
-    temperature.where {fields => fields.celsius.as[Double].get < fields.kelvin.as[Double].get}
-
-    def readTemperature(): Future[String] = readOne(temperature.build()) map {
-      _.fold(s"No ${client.temperature.name}") {_.toString}
+      () => for {values <- temperature("kelvin")} yield Try {values.max}
     }
 
-    private def temperature(name: String, at: String) =
-      readAll[Double](Selections.exact(s"temperature/$name") where {
-        _.location.as[String].get === at
-      })
+    def checkTemperature() {
+      readOne(temperature.build()) map {
+        _ map {
+          temperature =>
+            println(temperature)
+            val celsius = temperature.celsius.as[BigDecimal].get
+            if (celsius >= 50)
+              Seq(
+                "terminal-notifier",
+                "-title", "\"" + _configuration.name + '"',
+                "-message", "\"Temperature is " + celsius + '"'
+              ).!
+            println(_configuration.delimiter)
+        }
+      }
+    }
+
+    private def temperature(name: String) =
+      readAll[BigDecimal](Selections.exact(s"temperature/$name"))
   }
 
   override def shutdown() {
@@ -72,10 +83,7 @@ trait Client extends akka.System.Remote with Scheduling with Runnable {
 
   override def run() {
     println(configuration.delimiter)
-    every(configuration.period) {() =>
-      println(Await.result(client.readTemperature(), configuration.timeout))
-      println(configuration.delimiter)
-    }
+    every(configuration.period) {() => client.checkTemperature()}
   }
 
   private object _client extends Client
@@ -84,11 +92,11 @@ trait Client extends akka.System.Remote with Scheduling with Runnable {
 object Client {
   trait Configuration extends Scheduling.Configuration with akka.System.Remote.Configuration {
 
-    val name: String = "temperature-client"
+    val name: String = "raspberry-pi-client"
     val delimiter: String = "-" * 40
     def period: FiniteDuration = 2.seconds
 
     override val local = ActorSystem(name, ConfigFactory.load.getConfig(name))
-    override val remote = "akka.tcp://temperature-server@127.0.0.1:2552"
+    override val remote = "akka.tcp://raspberry-pi@192.168.0.23:2552"
   }
 }
